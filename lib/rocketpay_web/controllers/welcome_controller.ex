5c664808ad16f0e389c97f1be9c0821798d0502f
liviaribeiro14 <livia.ribeiro@venhapranuvem.com.br>
2021-04-02 20:20:45 -0300

    initial commit

---
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -0,0 +1,32 @@
+defmodule RocketpayWeb.WelcomeController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> IO.inspect()
+    |> json(reason)
+  end
+
+  def infinite_test(conn, _params) do
+    text = {:ok, %{result: "infinite"}}
+
+    text
+    |> handle_response(conn)
+  end
+
+end
