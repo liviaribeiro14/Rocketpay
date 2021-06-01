b1614cdd996bc23c69ccba470e84213d5aa0d2ec
Lívia Ribeiro <liviaribeiro14@gmail.com>
2021-06-01 19:06:26 -0300

    Setting async true

--- test/rocketpay/numbers_test.exs
+++ test/rocketpay/numbers_test.exs
@@ -1,10 +1,9 @@
 defmodule Rocketpay.NumbersTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   alias Rocketpay.Numbers
 
   describe "sum_from_file/1" do
-
     test "when there is a file with the given name, returns the sum of numbers" do
       response = Numbers.sum_from_file("numbers")
 
@@ -21,5 +20,4 @@ defmodule Rocketpay.NumbersTest do
       assert response == expected_response
     end
   end
-
 end
