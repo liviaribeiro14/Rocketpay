e8c0b31084d12815c68276e6961e1bd628f0d064
Lívia Ribeiro <ola@liviaribeiro.dev>
2021-06-06 16:48:50 -0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -1,15 +1,15 @@
 defmodule RocketpayWeb.Router do
   use RocketpayWeb, :router
 
-  import Plug.BasicAuth
+  # import Plug.BasicAuth
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  pipeline :auth do
-    plug :basic_auth, Application.compile_env(:rocketpay, :basic_auth)
-  end
+  # pipeline :auth do
+  #  plug :basic_auth, Application.compile_env(:rocketpay, :basic_auth)
+  # end
 
   scope "/api", RocketpayWeb do
     pipe_through :api
@@ -19,15 +19,19 @@ defmodule RocketpayWeb.Router do
     get "/test/infinite", WelcomeController, :infinite_test
 
     post "/users", UsersController, :create
-  end
-
-  scope "/api", RocketpayWeb do
-    pipe_through [:api, :auth]
 
     post "/accounts/:id/deposit", AccountsController, :deposit
     post "/accounts/:id/withdraw", AccountsController, :withdraw
     post "/accounts/transaction", AccountsController, :transaction
   end
+
+  # scope "/api", RocketpayWeb do
+  #  pipe_through [:api, :auth]
+
+  #  post "/accounts/:id/deposit", AccountsController, :deposit
+  #  post "/accounts/:id/withdraw", AccountsController, :withdraw
+  #  post "/accounts/transaction", AccountsController, :transaction
+  # end
 
   # Enables LiveDashboard only for development
   #
